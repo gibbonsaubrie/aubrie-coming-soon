6a5f6b0606a4598f0ae7f6ca45f83e242e951ce7
Matthew Silversmith <matthew.silversmith@gmail.com>
2023-05-21 19:15:26 -0400

    initial changes with aubrie

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
 $( document ).ready(function() {
 });
+
+$('#contact-us').click(function() {
+    var name = $("input#recipient-name").val();
+    var email = $("input#recipient-email").val();
+    var phone = $("input#recipient-phone").val();
+    var message = $("textarea#recipient-message").val();
+
+    var body = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+
+    location.href = "mailto:admin@executiveway.com?subject="+ encodeURI("New Submission From Coming Soon Website") +"&body=" + encodeURI(body);
+    var name = $("input#recipient-name").val('');
+    var email = $("input#recipient-email").val('');
+    var message = $("textarea#recipient-message").val('');
+});
